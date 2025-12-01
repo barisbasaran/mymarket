704c1d3f309235ef716f7eabdf7e44db99a2d89a
barisbasaran <basaran.baris@gmail.com>
2025-12-01 14:41:42 +0300

    setup

--- src/main/java/com/mymarket/web/jwt/JwtUnauthorizedHandler.java
+++ src/main/java/com/mymarket/web/jwt/JwtUnauthorizedHandler.java
@@ -1,5 +1,6 @@
 package com.mymarket.web.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -7,7 +8,6 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-import tools.jackson.databind.json.JsonMapper;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -31,7 +31,7 @@ public class JwtUnauthorizedHandler implements AuthenticationEntryPoint {
         body.put("message", authException.getMessage());
         body.put("path", request.getServletPath());
 
-        final JsonMapper mapper = new JsonMapper();
+        final ObjectMapper mapper = new ObjectMapper();
         mapper.writeValue(response.getOutputStream(), body);
     }
 }
